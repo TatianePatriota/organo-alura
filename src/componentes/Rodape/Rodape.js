8f06f1a317b54a411fd25e612e75ad4486920c9c
Tatiane Patriota <tatianepatriota1@gmail.com>
2022-10-07 14:54:26 -0300

    finalizado

--- src/componentes/Rodape/Rodape.js
+++ src/componentes/Rodape/Rodape.js
@@ -1,21 +1,35 @@
-import "./Rodape.css"
+import "./Rodape.css";
 
 function Rodape() {
   return (
     <footer className="rodape">
-      <div className="redes">
-        <a href="google.com"><img src="/imagens/fb.png" alt="Logo Facebook"></img></a>
-        <a href="google.com"><img src="/imagens/tw.png" alt="Logo Twitter"></img></a> 
-        <a href="google.com"><img src="/imagens/ig.png" alt="Logo Instagram"></img></a>
-      </div>
-      <div>
+      <section className="redes">
+        <ul>
+          <li>
+            <a href="google.com">
+              <img src="/imagens/fb.png" alt="Logo Facebook"></img>
+            </a>
+          </li>
+          <li>
+            <a href="google.com">
+              <img src="/imagens/tw.png" alt="Logo Twitter"></img>
+            </a>
+          </li>
+          <li>
+            <a href="google.com">
+              <img src="/imagens/ig.png" alt="Logo Instagram"></img>
+            </a>
+          </li>
+        </ul>
+      </section>
+      <section>
         <img src="/imagens/logo.png" alt="Logo organo"></img>
-      </div>
-      <div>
+      </section>
+      <section>
         <p>Desenvolvido por Alura.</p>
-      </div>
+      </section>
     </footer>
-  )
+  );
 }
 
 export default Rodape;
